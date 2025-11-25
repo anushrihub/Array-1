c7750ec5c5d57a9280e608b4cb64bef29ca88218
Anushri <sastikaranushri@gmail.com>
2025-11-25 13:51:08 -0500

    optimised solution

--- product_of_array_except_self.py
+++ product_of_array_except_self.py
@@ -1,43 +1,33 @@
 # https://leetcode.com/problems/product-of-array-except-self/description/
 
-# brutforce - O(n2) with two loops when i != j do product
+# Used the runnig product approach. In this with the loop find out the product of all elements to the left of each index stored in result array at the index position. Second loop is used to find the product of all element to the right of each index and multiply with the result array.    
+# Time Complexity: O(n) Space Complexity: O(1)
+
+
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
         n = len(nums)
         result = [0] * n
-        for i in range(n):
-            # to set the product equals to 1 for every begining of the i
-            product = 1
-            for j in range(n):
-                if i!=j:
-                    product *= nums[j]
-            print(product)
-            result[i] = product
+        # initializing the running product with 1
+        running_product = 1
+        # nothing at the left for the 1st element
+        result[0] = 1
+        # left side running product, as we set 0th element at 1 so starting from 1st position
+        for i in range(1,n):
+            # we want left side product so multiplying till the i - 1
+            running_product = running_product * nums[i - 1]
+            # at ith position we are storing the product of the left side numbers
+            result[i] = running_product
+
+        running_product = 1
+        # right side running product starting from second last element because we dont need to check for the last element as we already defined 1 
+        for i in range(n-2, -1, -1):
+            # we want right side product so multiplying from i + 1
+            running_product = running_product * nums[i + 1]
+            # at ith position we are storing the product of the right side numbers
+            result[i] = result[i] * running_product
+        
         return result
     
-
 solution = Solution()
 print(solution.productExceptSelf([1,2,3,4]))
-
-
-# class Solution:
-#     def productExceptSelf(self, nums: list[int]) -> list[int]:
-#         n = len(nums)
-#         result = [0] * n
-
-#         running_product = 1
-#         result[0] = 1
-#         # left side running product
-#         for i in range(1,n):
-#             running_product = running_product * nums[i - 1]
-#             result[i] = running_product
-
-#         running_product = 1
-#         for i in range(n-2, -1, -1):
-#             running_product = running_product * nums[i + 1]
-#             result[i] = result[i] * running_product
-        
-#         return result
-    
-# solution = Solution()
-# print(solution.productExceptSelf([1,2,3,4]))
